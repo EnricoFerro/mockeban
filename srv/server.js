78ceed01f707272accb3c706ecf0a38c590cf695
Enrico Ferro <enrico.ferro@dncsrl.com>
2023-04-18 11:16:59 +0200

    Set Cors disable

--- srv/server.js
+++ srv/server.js
@@ -3,6 +3,12 @@ const cov2ap = require("@sap/cds-odata-v2-adapter-proxy");
 const options = {
     path: "sap/opu/odata/sap"
 };
+cds.on('bootstrap', async app => {
+    app.use((req, res, next) => {
+        res.set('access-control-allow-origin', '*');
+        next();
+    });
+});
 cds.on("bootstrap", (app) => app.use(cov2ap(options)));
 //cds.odata.v2proxy.urlpath = '/sap/opu/odata/sap/ZBTP_POC1_GTW_SRV'
 module.exports = cds.server;
